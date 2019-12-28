dd7046e72b73ec333d90364aa759c5c85ba1ed97
cpharshaw <cpharshaw+git@gmail.com>
2019-12-28 15:06:14 -0500

    working on review form

--- client/src/components/MainWrapper/MainSection/ReviewSection/index.jsx
+++ client/src/components/MainWrapper/MainSection/ReviewSection/index.jsx
@@ -18,24 +18,25 @@ export class ReviewSection extends Component {
             flexDirection: "column",
             width: "100%",
             height: "calc(100vh - 84px)",
-            justifyContent: "center",
+            justifyContent: "space-around",
             alignItems: "center",
             alignContent: "center",
             margin: "0 auto",
             padding: "0",
             border: "0",
-            background: "#A2D4E6"
+            background: "whitesmoke"
           }
         }
       >
-
-        <input
+        <form
+          id="reviewForm"
           style={
             {
               display: "flex",
               width: "100%",
               height: "100%",
-              justifyContent: "center",
+              flexDirection: "column",
+              justifyContent: "space-around",
               alignItems: "center",
               alignContent: "center",
               margin: "0 auto",
@@ -45,7 +46,185 @@ export class ReviewSection extends Component {
           }
         >
 
-          {/* <div className="row flex-nowrap d-flex align-items-center">
+          <div
+            id="field01_wrapper"
+            style={
+              {
+                display: "flex",
+                width: "100%",
+                // height: "100%",
+                flexDirection: "row",
+                justifyContent: "space-around",
+                alignItems: "center",
+                alignContent: "center",
+                margin: "0 auto",
+                padding: "0",
+                border: "0",
+                background: "grey"
+              }
+            }
+          >
+            <label
+              htmlFor="field01_label"
+              style={
+                {
+                  display: "flex",
+                  // flexGrow: "1",
+                  width: "35%",
+                  height: "100%",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                  alignContent: "center",
+                  margin: "0 auto",
+                  padding: "0",
+                  border: "1px solid #44aacc",
+                  background: "red"
+                }
+              }
+            >
+              Shit
+            </label>
+
+            <div
+              id="field01_inputs"
+              style={
+                {
+                  display: "flex",
+                  // flexGrow: "3",
+                  width: "100%",
+                  height: "100%",
+                  flexDirection: "row",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                  alignContent: "center",
+                  margin: "0 auto",
+                  padding: "0",
+                  border: "0",
+                  background: "blue"
+                }
+              }
+            >
+              <input
+                id="field01_input01"
+                value="field01_input01_value"
+                name="field01_input_name"
+                type="radio"
+                style={
+                  {
+                    display: "flex",
+                    // flexGrow: "1",
+                    // width: "65%",
+                    height: "100%",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    alignContent: "center",
+                    alignSelf: "center",
+                    margin: "0 auto",
+                    padding: "0",
+                    border: "0",
+                    background: "green"
+                  }
+                }
+              >
+              </input>
+              <input
+                id="field01_input02"
+                type="radio"
+                value="field01_input02_value"
+                name="field01_input_name"
+                style={
+                  {
+                    display: "flex",
+                    // flexGrow: "1",
+                    // width: "65%",
+                    height: "100%",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    alignContent: "center",
+                    margin: "0 auto",
+                    padding: "0",
+                    border: "0",
+                    background: "yellow"
+                  }
+                }
+              >
+              </input>
+
+              <input
+                id="field01_input03"
+                type="radio"
+                value="field01_input03_value"
+                name="field01_input_name"
+                style={
+                  {
+                    display: "flex",
+                    // flexGrow: "1",
+                    // width: "65%",
+                    height: "100%",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    alignContent: "center",
+                    margin: "0 auto",
+                    padding: "0",
+                    border: "0",
+                    background: "yellow"
+                  }
+                }
+              >
+              </input>
+
+              <input
+                id="field01_input04"
+                type="radio"
+                value="field01_input04_value"
+                name="field01_input_name"
+                style={
+                  {
+                    display: "flex",
+                    // flexGrow: "1",
+                    // width: "65%",
+                    height: "100%",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    alignContent: "center",
+                    margin: "0 auto",
+                    padding: "0",
+                    border: "0",
+                    background: "yellow"
+                  }
+                }
+              >
+              </input>
+
+              <input
+                id="field01_input05"
+                type="radio"
+                value="field01_input05_value"
+                name="field01_input_name"
+                style={
+                  {
+                    display: "flex",
+                    // flexGrow: "1",
+                    // width: "65%",
+                    height: "100%",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    alignContent: "center",
+                    margin: "0 auto",
+                    padding: "0",
+                    border: "0",
+                    background: "yellow"
+                  }
+                }
+              >
+              </input>
+
+            </div>
+          </div>
+        </form>
+
+        {/* 
+          <div className="row flex-nowrap d-flex align-items-center">
             <div className="col-sm-4 categoryLabel">Cleanliness:</div>
             <div className="col-sm-8 star-rating">
               <div className="star-rating__wrap ">
@@ -61,84 +240,10 @@ export class ReviewSection extends Component {
                 <label className="star-rating__ico fa fa-star-o fa-md" htmlFor="clean-rating-1" title="1 out of 5 stars"></label>
               </div>
             </div>
-          </div> */}
+          </div> 
+        */}
 
-        </input>
-
-        <input
-          style={
-            {
-              display: "flex",
-              width: "100%",
-              height: "100%",
-              justifyContent: "center",
-              alignItems: "center",
-              alignContent: "center",
-              margin: "0 auto",
-              padding: "0",
-              border: "0"
-            }
-          }
-        >
-          {/* ReviewSection2 */}
-        </input>
-
-        <div
-          style={
-            {
-              display: "flex",
-              width: "100%",
-              height: "100%",
-              justifyContent: "center",
-              alignItems: "center",
-              alignContent: "center",
-              margin: "0 auto",
-              padding: "0",
-              border: "0"
-            }
-          }
-        >
-          ReviewSection3
-        </div>
-
-        <div
-          style={
-            {
-              display: "flex",
-              width: "100%",
-              height: "100%",
-              justifyContent: "center",
-              alignItems: "center",
-              alignContent: "center",
-              margin: "0 auto",
-              padding: "0",
-              border: "0"
-            }
-          }
-        >
-          ReviewSection4
-        </div>
-
-        <div
-          style={
-            {
-              display: "flex",
-              width: "100%",
-              height: "100%",
-              justifyContent: "center",
-              alignItems: "center",
-              alignContent: "center",
-              margin: "0 auto",
-              padding: "0",
-              border: "0"
-            }
-          }
-        >
-          ReviewSection5
-        </div>
-
-
-      </div>
+      </div >
     )
   }
 }
